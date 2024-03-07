926b37b7af9b409fe1db54f393ded01b9e81ac9b
Danish-Ehsan <danish.ehsan321@gmail.com>
2024-03-07 16:52:15 -0500

    Added mock list data

---
+++ src/data/index.ts
@@ -0,0 +1,54 @@
+const todoLists = [
+  {
+    listName: "Groceries",
+    id: 1,
+    listItems: [
+      {
+        itemName: "Meat",
+        id: 1,
+        completed: false,
+      },
+      {
+        itemName: "Vegetables",
+        id: 2,
+        completed: false,
+      },
+      {
+        itemName: "Fruit",
+        id: 3,
+        completed: false,
+      },
+      {
+        itemName: "Snacks",
+        id: 4,
+        completed: true,
+      },
+    ],
+  },
+  {
+    listName: "Chores",
+    id: 1,
+    listItems: [
+      {
+        itemName: "Clean the house",
+        id: 1,
+        completed: false,
+      },
+      {
+        itemName: "Cook dinner",
+        id: 2,
+        completed: false,
+      },
+      {
+        itemName: "Water plants",
+        id: 3,
+        completed: false,
+      },
+      {
+        itemName: "Email friends",
+        id: 4,
+        completed: true,
+      },
+    ],
+  },
+];
