5c8e7bf98c2fdd2b6bdab261a860542cf43f5fdf
Danish-Ehsan <danish.ehsan321@gmail.com>
2024-03-08 12:45:52 -0500

    Added TodosList type and initial Todos from mock data

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import AllLists from "./components/AllLists";
 import List from "./components/List";
-import { ViewState } from "./types";
+import { ViewState, TodosList } from "./types";
+import { todoLists as initialTodos } from "./data";
 
 function App() {
   const [view, setView] = useState<ViewState>("allLists");
   const [currentListId, setCurrentListId] = useState(0);
+  const [todoList, setTodoList] = useState<TodosList>(initialTodos);
 
   return <>{view === "allLists" ? <AllLists onSetView={setView} onListChange={setCurrentListId} /> : <List onSetView={setView} listId={currentListId} />}</>;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
 export type ViewState = "allLists" | "singleList";
+
+export type TodosList = {
+  listName: string;
+  id: number;
+  listItems: {
+    itemName: string;
+    id: number;
+    completed: boolean;
+  }[];
+}[];
