76e8277abd650d5e553aae05f05304859d221f6e
Danish-Ehsan <danish.ehsan321@gmail.com>
2024-03-22 13:17:58 -0400

    ListItem now resizes to the height of the content

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef } from "react";
 import { AllTodoLists, HandleListItemChangeType, HandleRemoveItem, HandleMarkItem } from "../../types";
 import allListStyles from "../AllLists/AllLists.module.scss";
 import listStyles from "../List/List.module.scss";
@@ -12,13 +13,22 @@ type ListItemProps = {
 };
 
 export default function ListItem({ listItem, listId, onMarkItem, onListItemChange, onRemoveItem }: ListItemProps) {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "";
+      textareaRef.current.style.height = textareaRef.current.scrollHeight + "px";
+    }
+  }, [listItem]);
+
   return (
-    <div className={allListStyles.listRow} key={listItem.id.toString()}>
+    <div className={`${allListStyles.listRow} ${allListStyles["listRow--align-start"]}`} key={listItem.id.toString()}>
       <label className={listStyles.checkboxLabel}>
         <input className={listStyles.checkbox} type="checkbox" checked={listItem.completed} onChange={() => onMarkItem(listId, listItem.id, !listItem.completed)} />
         <span className={listStyles.customCheckmark}></span>
       </label>
-      <textarea className={`${listStyles.listTextarea} ${listStyles.listItem} ${listItem.completed ? listStyles["is-completed"] : ""}`} rows={1} onChange={(e) => onListItemChange(listId, e.target.value, listItem.id)}>
+      <textarea ref={textareaRef} className={`${listStyles.listTextarea} ${listStyles.listItem} ${listItem.completed ? listStyles["is-completed"] : ""}`} rows={1} onChange={(e) => onListItemChange(listId, e.target.value, listItem.id)}>
         {listItem.itemName}
       </textarea>
       <button onClick={() => onRemoveItem(listId, listItem.id)} className={`${allListStyles.trashBtn} ${allListStyles["trashBtn--listItem"]}`}>
