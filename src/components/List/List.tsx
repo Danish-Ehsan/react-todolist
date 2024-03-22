569411cf1c51a4a73c776101961a45d81628074a
Danish-Ehsan <danish.ehsan321@gmail.com>
2024-03-22 11:19:48 -0400

    Converted inputs to textareas

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -22,7 +22,9 @@ export default function List({ list, onSetView, onListTitleChange, onListItemCha
           <input className={styles.checkbox} type="checkbox" checked={listItem.completed} onChange={() => onMarkItem(list.id, listItem.id, !listItem.completed)} />
           <span className={styles.customCheckmark}></span>
         </label>
-        <input className={`${styles.listInput} ${styles.listItem} ${listItem.completed ? styles["is-completed"] : ""}`} type="text" value={listItem.itemName} onChange={(e) => onListItemChange(list.id, e.target.value, listItem.id)} />
+        <textarea className={`${styles.listTextarea} ${styles.listItem} ${listItem.completed ? styles["is-completed"] : ""}`} rows={1} onChange={(e) => onListItemChange(list.id, e.target.value, listItem.id)}>
+          {listItem.itemName}
+        </textarea>
         <button onClick={() => onRemoveItem(list.id, listItem.id)} className={`${allListStyles.trashBtn} ${allListStyles["trashBtn--listItem"]}`}>
           <TrashIcon className={allListStyles.trashIcon} />
         </button>
@@ -32,14 +34,15 @@ export default function List({ list, onSetView, onListTitleChange, onListItemCha
 
   return (
     <>
-      <input
-        className={`${styles.listTitle} ${styles.listInput}`}
-        type="text"
-        value={list?.listName}
+      <textarea
+        className={`${styles.listTitle} ${styles.listTextarea}`}
+        rows={1}
         onChange={(e) => {
           onListTitleChange(list.id, e.target.value);
         }}
-      />
+      >
+        {list?.listName}
+      </textarea>
       {listItemElems}
       <button className="button" onClick={() => onAddItem(list.id)}>
         + Add Item
