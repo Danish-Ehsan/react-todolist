bc95c5b637c02dee76bb4f77caf0eed54ae0e1da
Danish <danish.ehsan321@gmail.com>
2024-05-03 00:31:57 -0400

    Updated default list IDs

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,52 +1,52 @@
 export const todoLists = [
   {
     listName: "Groceries",
-    id: 1,
+    id: 1714710594045,
     listItems: [
       {
         itemName: "Meat",
-        id: 1,
+        id: 1714710594046,
         completed: false,
       },
       {
         itemName: "Vegetables",
-        id: 2,
+        id: 1714710594047,
         completed: false,
       },
       {
         itemName: "Fruit",
-        id: 3,
+        id: 1714710594048,
         completed: false,
       },
       {
         itemName: "Snacks",
-        id: 4,
+        id: 1714710594049,
         completed: true,
       },
     ],
   },
   {
     listName: "Chores",
-    id: 2,
+    id: 1714710594050,
     listItems: [
       {
         itemName: "Clean the house",
-        id: 1,
+        id: 1714710594051,
         completed: false,
       },
       {
         itemName: "Cook dinner",
-        id: 2,
+        id: 1714710594052,
         completed: false,
       },
       {
         itemName: "Water plants",
-        id: 3,
+        id: 1714710594053,
         completed: false,
       },
       {
         itemName: "Email friends",
-        id: 4,
+        id: 1714710594054,
         completed: true,
       },
     ],
