aa96b83cb2ee03aef62e30eb3570481c860c4e44
Danish <danish.ehsan321@gmail.com>
2024-05-05 22:57:08 -0400

    Removed unused functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useContext } from "react";
 import styles from "./App.module.scss";
 import AllLists from "./components/AllLists/AllLists";
 import List from "./components/List/List";
-import { ViewState, HandleListTitleChangeType, HandleListItemChangeType, HandleRemoveList, HandleRemoveItem, HandleAddList, HandleAddItem, HandleMarkItem } from "./types";
+import { ViewState } from "./types";
 import { ListsContext, ListsProvider } from "./providers/ListProvider.tsx";
 
 function App() {
@@ -16,64 +16,6 @@ function App() {
     } else {
       return todosLists.findIndex((todoList) => todoList.id === currentListId)!;
     }
-  };
-
-  const handleListTitleChange: HandleListTitleChangeType = (listId, newTitle) => {
-    dispatch({
-      type: "title-changed",
-      listId: listId,
-      newTitle: newTitle,
-    });
-  };
-
-  const handleListItemChange: HandleListItemChangeType = (listId, newItemName, itemId) => {
-    dispatch({
-      type: "item-changed",
-      listId: listId,
-      newItemName: newItemName,
-      itemId: itemId,
-    });
-  };
-
-  const handleRemoveList: HandleRemoveList = (listId) => {
-    dispatch({
-      type: "list-removed",
-      listId: listId,
-    });
-  };
-
-  const handleRemoveItem: HandleRemoveItem = (listId, itemId) => {
-    dispatch({
-      type: "item-removed",
-      listId: listId,
-      itemId: itemId,
-    });
-  };
-
-  const handleAddList: HandleAddList = () => {
-    dispatch({
-      type: "list-added",
-      listItemId: Date.now()
-    });
-
-    setView("singleList");
-  };
-
-  const handleAddItem: HandleAddItem = (listId, listItemId) => {
-    dispatch({
-      type: "item-added",
-      listId,
-      listItemId
-    });
-  };
-
-  const handleMarkItem: HandleMarkItem = (listId, itemId, completed) => {
-    dispatch({
-      type: "item-marked",
-      listId: listId,
-      itemId: itemId,
-      completed: completed,
-    });
   };
 
   return (
