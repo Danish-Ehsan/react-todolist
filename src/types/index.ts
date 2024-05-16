3eff98015f50f7f7b39196afbaf56847e65d3fef
Danish <danish.ehsan321@gmail.com>
2024-05-16 00:11:37 -0400

    Removed unused Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,17 +12,3 @@ export type AllTodoLists = {
     completed: boolean;
   }[];
 }[];
-
-export type HandleListTitleChangeType = (listId: number, newTitle: string) => void;
-
-export type HandleListItemChangeType = (listId: number, newItemName: string, itemId: number) => void;
-
-export type HandleRemoveList = (listId: number) => void;
-
-export type HandleRemoveItem = (listId: number, itemId: number) => void;
-
-export type HandleAddList = (listItemId: number) => void;
-
-export type HandleAddItem = (listId: number, listItemId: number) => void;
-
-export type HandleMarkItem = (listId: number, itemId: number, complete: boolean) => void;
