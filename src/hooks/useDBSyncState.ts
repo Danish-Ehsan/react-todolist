2a01137c3d80d3d043a5d941f0bf20b53d77bdf1
Danish <danish.ehsan321@gmail.com>
2024-05-22 22:10:53 -0400

    Update to useDBSyncState implementation
    The useDBSyncState hook now provides a custom setter function instead of using the state setter directly. The custom function only sets the DBError state if the DBError.error state is false.

--- src/hooks/useDBSyncState.ts
+++ src/hooks/useDBSyncState.ts
@@ -1,12 +1,32 @@
 import { useContext } from "react";
-import { DBSyncStateContext  } from "../providers/DBSyncStateProvider";
+import { DBSyncStateContext } from "../providers/DBSyncStateProvider";
+import useDBError from "./useDBError";
 
-export default function useDBSyncState() {
-  const dbSyncState = useContext(DBSyncStateContext);
+type DBSyncStateSetter = (value: React.SetStateAction<boolean>) => void;
 
-  if (!dbSyncState) {
+export default function useDBSyncState(): [boolean, DBSyncStateSetter] {
+  const DBSyncStateValue = useContext(DBSyncStateContext);
+
+  if (DBSyncStateValue === null) {
     throw new Error('useDBSyncState was used outside of its Provider');
   }
 
-  return dbSyncState;
+  const [DBSyncState, setDBSyncState] = DBSyncStateValue;
+  const [DBError] = useDBError();
+
+  
+
+  // Provide a custom setter function for DBSyncState state so that the value can't be changed to "true" if there is an existing DBError.
+  const DBSyncStateSetter: DBSyncStateSetter = (value) => {
+    console.log(DBError);
+    if (DBError.error) {
+      console.log('There is a DB Error');
+      return;
+    } else {
+      console.log({value});
+      setDBSyncState(value);
+    }
+  }
+
+  return [DBSyncState, DBSyncStateSetter];
 }
